2bdd846c6b51e037ffa371a227e0652276c1f7c4
FM2805 <florian.messner@zoho.com>
2017-02-04 15:15:07 +0100

    Update Variable Creation.R

--- Variable Creation.R
+++ Variable Creation.R
@@ -2,7 +2,7 @@
 # Basic code for creating variable names (without assigning them) while avoiding a loop 
 ########################
 
-# Create index_1 to index_3 as a (future variable) - do not assign yet
+# Create index_1 to index_3 as a potential (future) variable - is not yet assigned
 l_selected <- as.list(1:length(indices))
 l_str <- list("Index")
 s_var <- as.list(paste(l_str,l_selected,sep=""))
